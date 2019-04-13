90291e473f70ff61a4555d38a47741acbdb88b6f
nosyarg <york17a@ncssm.edu>
2019-04-13 15:15:16 -0400

    got file i/o working locally

--- crypto/crypto.js
+++ crypto/crypto.js
@@ -7,8 +7,18 @@ function hash(infile)
         var data = fs.readFileSync(infile,'ascii');
         return sjcl.codec.hex.fromBits((new sjcl.hash.sha256()).update(data).finalize());
 }
-var datadict = {};
+var jsonstr = fs.readFileSync("writefile.txt",'utf-8');; 
+var datadict = JSON.parse(jsonstr);
 var stupidstring = process.argv[2];
 var stupidhash = hash(stupidstring);
-datadict[stupidhash] = "TODAY";
-console.log(datadict[stupidhash]);
+if(!(stupidhash in datadict))
+{
+        console.log("NEVER")
+        datadict[stupidhash] = Date.now();
+}
+else
+{
+        console.log((new Date(datadict[stupidhash])).toLocaleString("en-US"));
+}
+jsonstr = JSON.stringify(datadict);
+fs.writeFileSync('writefile.txt', jsonstr);
